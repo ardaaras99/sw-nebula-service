99d438a58475e22c75ffbe673dae4fcaff876f10
Arda Can Aras <ardaaras99@gmail.com>
2025-06-27 10:32:03 +0300

    chore: initial commit

--- sw_nebula_service/models/nodes.py
+++ sw_nebula_service/models/nodes.py
@@ -20,14 +20,23 @@ class OntologyNode(BaseNebulaNode):
 
 
 class PdfNode(BaseNebulaNode):
+    # Metadata
     user_id: str
+    node_id: str
     pdf_file_hash: str
     pdf_file_name: str
-    lib_name: str
-    ontology_name: str
     time_of_upload: datetime
     file_load_status: bool
     kg_extraction_status: bool
+
+    # AI generated
+    ai_lib_name: str
+    ai_ontology_name: str
+    ai_reasoning_for_classification: str
+
+    # User chosen
+    user_chosen_lib_name: str
+    user_chosen_ontology_name: str
 
 
 PREDEFINED_NODE_CLASSES = [
